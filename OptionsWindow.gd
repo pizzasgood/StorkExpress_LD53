2bdbfbb5f39632ac678a0aba4ab53f23ce4857cf
pizzasgood <pizzasgood@gmail.com>
2023-04-29 19:46:05 -0400

    Improve graphics performance

--- OptionsWindow.gd
+++ OptionsWindow.gd
@@ -4,6 +4,8 @@ extends PopupPanel
 @onready var master_volume: Slider = find_child("MasterVolume")
 @onready var sfx_volume: Slider = find_child("SFXVolume")
 @onready var music_volume: Slider = find_child("MusicVolume")
+@onready var vsync: CheckBox = find_child("VSync")
+@onready var max_fps: SpinBox = find_child("MaxFPS")
 @onready var screen_darken: ColorRect = get_tree().current_scene.find_child("ScreenDarken")
 
 
@@ -11,6 +13,8 @@ func update_widgets() -> void:
 	master_volume.value = Globals.master_volume
 	sfx_volume.value = Globals.sfx_volume
 	music_volume.value = Globals.music_volume
+	vsync.button_pressed = DisplayServer.window_get_vsync_mode() == DisplayServer.VSYNC_ENABLED
+	max_fps.value = Engine.max_fps
 
 
 func _on_OptionsWindow_about_to_show() -> void:
@@ -32,6 +36,14 @@ func _on_SFXVolume_value_changed(value) -> void:
 
 func _on_MusicVolume_value_changed(value) -> void:
 	Globals.music_volume = value
+
+
+func _on_VSync_toggled(button_pressed):
+	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED if button_pressed else DisplayServer.VSYNC_DISABLED)
+
+
+func _on_MaxFPS_value_changed(value):
+	Engine.max_fps = value
 
 
 func _on_ResumeButton_pressed() -> void:
